e8a830ca0e0862f59062db152850c045df1d6f90
Mauricio Medina <minimiGamesAM@gmail.com>
2023-11-16 22:17:23 -0500

    Implementing no linear projection to resolve interpenetration instead of reverse the solver due to avoid introducing additional friction.

--- EngineG/src/physicsEngine/CollisionResponse.cpp
+++ EngineG/src/physicsEngine/CollisionResponse.cpp
@@ -112,6 +112,124 @@ Contact::Contact()
 	body[1] = nullptr;
 }
 
+void Contact::applyPositionChange(glm::vec3 linearChange[2],
+	glm::vec3 angularChange[2],
+	float penetration,
+	float dt)
+{
+	const float angularLimit = 0.2f;
+	float angularMove[2];
+	float linearMove[2];
+
+	float totalInertia = 0;
+	float linearInertia[2];
+	float angularInertia[2];
+
+	// We need to work out the inertia of each object in the direction
+	// of the contact normal, due to angular inertia only.
+	for (unsigned i = 0; i < 1; i++) if (body[i])
+	{
+		glm::mat3 inverseInertiaTensor = body[i]->mIinv;
+
+		// Use the same procedure as for calculating frictionless
+		// velocity change to work out the angular inertia.
+		float term3 = glm::dot(contactNormal, glm::cross(inverseInertiaTensor * glm::cross(relContactPoint, contactNormal), relContactPoint));
+		angularInertia[i] = term3;
+
+		// The linear component is simply the inverse mass
+		float term1 = 1 / body[i]->mMass;
+		linearInertia[i] = term1;
+
+		// Keep track of the total inertia from all components
+		totalInertia += linearInertia[i] + angularInertia[i];
+
+		// We break the loop here so that the totalInertia value is
+		// completely calculated (by both iterations) before
+		// continuing.
+	}
+
+	// Loop through again calculating and applying the changes
+	for (unsigned i = 0; i < 1; i++) if (body[i])
+	{
+		glm::mat3 inverseInertiaTensor = body[i]->mIinv;
+
+		// The linear and angular movements required are in proportion to
+		// the two inverse inertias.
+		float sign = (i == 0) ? 1 : -1;
+		angularMove[i] =
+			sign * contactDepth * (angularInertia[i] / totalInertia);
+		linearMove[i] =
+			sign * contactDepth * (linearInertia[i] / totalInertia);
+
+		// To avoid angular projections that are too great (when mass is large
+		// but inertia tensor is small) limit the angular move.
+
+		glm::vec3 projection = relContactPoint;
+		projection += contactNormal * (glm::dot(-relContactPoint, contactNormal));
+
+		// Use the small angle approximation for the sine of the angle (i.e.
+		// the magnitude would be sine(angularLimit) * projection.magnitude
+		// but we approximate sine(angularLimit) to angularLimit).
+		float maxMagnitude = angularLimit * projection.length();
+
+		if (angularMove[i] < -maxMagnitude)
+		{
+			float totalMove = angularMove[i] + linearMove[i];
+			angularMove[i] = -maxMagnitude;
+			linearMove[i] = totalMove - angularMove[i];
+		}
+		else if (angularMove[i] > maxMagnitude)
+		{
+			float totalMove = angularMove[i] + linearMove[i];
+			angularMove[i] = maxMagnitude;
+			linearMove[i] = totalMove - angularMove[i];
+		}
+
+		// We have the linear amount of movement required by turning
+		// the rigid body (in angularMove[i]). We now need to
+		// calculate the desired rotation to achieve that.
+		if (angularMove[i] == 0)
+		{
+			// Easy case - no angular movement means no rotation.
+			angularChange[i] = glm::vec3(0);
+		}
+		else
+		{
+			// Work out the direction we'd like to rotate in.
+			glm::vec3 targetAngularDirection =
+				glm::cross(relContactPoint, contactNormal);
+
+			// Work out the direction we'd need to rotate to achieve that
+			angularChange[i] =
+				(inverseInertiaTensor * targetAngularDirection) *
+				(angularMove[i] / angularInertia[i]);
+		}
+
+		// Velocity change is easier - it is just the linear movement
+		// along the contact normal.
+		linearChange[i] = contactNormal * linearMove[i];
+
+		// Now we can start to apply the values we've calculated.
+		// Apply the linear movement
+		body[i]->mX += contactNormal * linearMove[i];
+
+		// And the change in orientation
+		glm::quat Wquad(0.0f, angularChange[i][0], angularChange[i][1], angularChange[i][2]);
+		glm::quat qdot = 0.5f * (Wquad * body[i]->mQ);
+		body[i]->mQ += qdot;
+
+
+		// We need to calculate the derived data for any body that is
+		// asleep, so that the changes are reflected in the object's
+		// data. Otherwise the resolution will not change the position
+		// of the object, and the next collision detection round will
+		// have the same penetration.
+		// 
+		//if (!body[i]->getAwake()) body[i]->calculateDerivedData();
+		body[i]->calculateInternalData();
+	}
+}
+
 CollisionResponse::CollisionResponse()
 {
 	mContacts = new Contact[MaxContacts];
@@ -215,6 +333,11 @@ void CollisionResponse::update(std::vector<RigidBody*>& bodies, float* y, float*
 			{
 				Contact* c = &(mContacts[index]);
 				resolveInterpenetration(c->body[0], vertex[c->localContactId], y, ydot, 0, dt, dt, solver);
+
+				glm::vec3 linearChange[2], angularChange[2];
+				(&mContacts[index])->applyPositionChange(linearChange,
+					angularChange,
+					max, dt);
 
 				//update contacts due to the resolving interpenetration
 				for (int j = 0; j < distance; j++)
