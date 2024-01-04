2634a77b89035cc85bc0873506fc923caa8fde94
Mauricio Medina <minimiGamesAM@gmail.com>
2024-01-03 19:11:27 -0500

    Implementing resolve penetration from (Contact Force for non penetrating rigid bodies).

--- EngineG/src/physicsEngine/CollisionResponse.cpp
+++ EngineG/src/physicsEngine/CollisionResponse.cpp
@@ -7,6 +7,50 @@
 
 namespace
 {
+	template <typename scalar_type>
+	void cholesky_factor(const std::vector<scalar_type>& A, const std::vector<scalar_type>& b, std::vector<scalar_type>& x)
+	{
+		size_t n = std::sqrt(A.size());
+		std::vector<scalar_type> result(A.size(), 0.0f);
+
+		for (size_t i = 0; i < n; ++i)
+		{
+			for (size_t k = 0; k < i; ++k)
+			{
+				scalar_type value = A[k + i * n];
+				for (size_t j = 0; j < k; ++j)
+					value -= result[j + i * n] * result[j + k * n];
+				result[k + i * n] = value / result[k + k * n];
+			}
+			scalar_type value = A[i + i * n];
+			for (size_t j = 0; j < i; ++j)
+				value -= result[j + i * n] * result[j + i * n];
+			result[i + i * n] = std::sqrt(value);
+		}
+
+		//forward
+		x.resize(n);
+		for (int i = 0; i < n; ++i)
+		{
+			scalar_type val(0.0f);
+			for (int j = 0; j < i; ++j)
+			{
+				val += result[j + i * n] * x[j];
+			}
+			x[i] = (b[i] - val) / result[i + i * n];
+		}
+
+		//backward
+		for (int i = n - 1; i >= 0; --i)
+		{
+			scalar_type val(0.0f);
+			for (int j = n - 1; j > i; --j)
+			{
+				val += result[i + j * n] * x[j];
+			}
+			x[i] = (x[i] - val) / result[i + i * n];
+		}
+	}
 
 	void velocity(RigidBody* b, const glm::vec3& P, glm::vec3& v)
 	{
@@ -106,6 +150,185 @@ namespace
 			}
 		}
 	}
+
+	//////////////////////
+	int colliding(Contact* contact)
+	{
+		glm::vec3 v = desiredVelocity(contact);
+
+		float velRel = glm::dot(v, contact->contactNormal);
+		
+		float THRESHOLD = 2.0;
+		if (velRel > THRESHOLD)
+			return 0; // moving away
+		else if (velRel > -THRESHOLD)
+			return 1; // resting contact
+		else
+			return 2; // contact
+	}
+
+	//////////////////////
+
+	//template <typename scalar_type>
+	//void cholesky_factor(const std::vector<scalar_type>& input, std::vector<scalar_type>& result)
+	//{
+	//	size_t n = input.size() / 2;
+	//	result.resize(input.size());
+	//
+	//	for (size_t i = 0; i < n; ++i) {
+	//		for (size_t k = 0; k < i; ++k) {
+	//			scalar_type value = input[k + i * n];//(i, k);
+	//			for (size_t j = 0; j < k; ++j)
+	//				value -= result[j + i * n] * result[j + k * n];
+	//			result[k + i * n] = value / result[k + k * n];
+	//		}
+	//		scalar_type value = input[i + i * n];
+	//		for (size_t j = 0; j < i; ++j)
+	//			value -= result[j + i * n] * result[j + i * n];
+	//		result[i + i * n] = std::sqrt(value);
+	//	}
+	//}
+
+	static float THRESHOLD_ACC = 1.0f;
+
+	void compute_b(std::vector<Contact*>& restingContacts, int i, std::vector<float>& b)
+	{
+		Contact* c_i = restingContacts[i];
+		RigidBody* rigid = c_i->body[0];
+
+		glm::vec3 rA = c_i->contactPoint - rigid->mX;
+		glm::vec3 ni = c_i->contactNormal;
+		
+		glm::vec3 term3 = glm::cross(rigid->mW, glm::cross(rigid->mW, rA));
+		glm::vec3 term4 = glm::cross(rigid->mL, glm::cross(rigid->mW, rA)) * rigid->mIinv;
+
+		b[i] = glm::dot(term3 + term4, ni);
+	}
+
+	void computeA(std::vector<Contact*>& restingContacts, int i, int j, std::vector<float>& A)
+	{
+		Contact* c_i = restingContacts[i];
+		Contact* c_j = restingContacts[j];
+
+		RigidBody* b = c_i->body[0];
+		glm::vec3 rA = c_i->contactPoint - b->mX;
+
+		glm::vec3 nj = c_j->contactNormal;
+		glm::vec3 pj = c_j->contactPoint;
+
+		float term1 = 1.0f / b->mMass;
+		float term3 = glm::dot(nj, b->mIinv * glm::cross(glm::cross((pj - b->mX), nj), rA));
+		float term = term1 + term3;
+		
+		A[j + restingContacts.size() * i] = term3;
+	}
+	
+	void fdirection(std::vector<float>& A, std::vector<Contact*>& restingContacts, int d, const std::vector<int>& U, std::vector<float>& Df)
+	{
+		Df[d] = 1.0f;
+		std::vector<float> Acc;
+		std::vector<float> Acd;
+		
+		int n = restingContacts.size();
+
+		int row = 0;
+		for (int i = 0; i < A.size(); i++)
+		{
+			int column = i % n;
+			if (column == n - 1)
+				row++;
+
+			if (A[i] > -THRESHOLD_ACC && A[i] < THRESHOLD_ACC)
+			{
+				if (column == d)
+				{
+					Acd.push_back(A[i]);
+				}
+			}
+		}
+
+		Acc.resize(U.size() * U.size());
+		for (int i = 0; i < U.size(); i++)
+		{
+			for (int j = 0; j < U.size(); j++)
+			{
+				Acc[j + U.size() * i] = A[U[j] + restingContacts.size() * U[i]];
+			}
+		}
+
+		std::vector<float> x;
+		cholesky_factor(Acc, Acd, x);
+		for (int j = 0; j < x.size(); j++)
+		{
+			Df[U[j]] = x[j];
+		}
+	}
+
+	void maxstep(std::vector<float>& f, std::vector<float>& a)
+	{
+
+	}
+
+	void drive_to_zero(std::vector<float>& A, std::vector<float>& a, std::vector<Contact*>& restingContacts, int d)
+	{
+		std::vector<float> Df(restingContacts.size(), 0.0f);
+		std::vector<float> Da(restingContacts.size(), 0.0f);
+
+		std::vector<int> U;
+
+		for (int i = 0; i < restingContacts.size(); i++)
+		{
+			if (A[i] > -THRESHOLD_ACC && A[i] < THRESHOLD_ACC)
+			{
+				U.push_back(i);
+			}
+		}
+
+		fdirection(A, restingContacts, d, U, Df);
+
+		for (int i = 0; i < restingContacts.size(); i++)
+		{
+			for (int j = 0; j < restingContacts.size(); j++)
+			{
+				Da[i] += A[j + restingContacts.size() * i] * Df[j];
+			}
+		}
+
+	}
+
+	void restingContactsHandled(std::vector<Contact*>& restingContacts)
+	{
+		std::vector<float> A;
+		std::vector<float> b;
+		std::vector<float> f;
+		A.resize(restingContacts.size() * restingContacts.size());
+		b.resize(restingContacts.size());
+		f.resize(restingContacts.size());
+
+		for (int i = 0; i < restingContacts.size(); i++)
+		{
+			compute_b(restingContacts, i, b);
+			for (int j = 0; j < restingContacts.size(); j++)
+			{
+				computeA(restingContacts, i, j, A);
+			}
+		}
+
+		std::vector<float> a = b;
+		//bool terminate = false;
+		//while ()
+		{
+			for (int i = 0; i < restingContacts.size(); i++)
+			{
+				if (A[i] <= -THRESHOLD_ACC)
+				{
+					drive_to_zero(A, a, restingContacts, i);
+					break;
+				}
+			}
+		};
+	}
+	//////////////////////
 }
 
 CollisionResponse::CollisionResponse()
@@ -119,100 +342,20 @@ void CollisionResponse::update(CollisionData* cData, std::vector<CollisionPrimit
 	//collision detection
 	//generateContacts(bodies);
 	if (cData->contactCount > 0)
-	{
-		for (int i = 0; i < primitives.size(); i++)
+	{		
+		std::vector<Contact*> restingContacts;
+		for (int j = 0; j < cData->contactCount; j++)
 		{
-			RigidBody* b = primitives[i]->body;
+			Contact* c = &(cData->contactArray[j]);
 
-			float max = 0.0f;
-			int index = -1;
+			int typeOfC = colliding(c);
 
-			for (int j = 0; j < cData->contactCount; j++)
-			{
-				Contact* c = &(cData->contactArray[j]);
-
-				if (c->body[0] == b)
-				{
-					if (c->contactDepth > max)
-					{
-						max = c->contactDepth;
-						index = j;
-					}
-				}
-			}
-
-			if (index != -1)
-			{
-				Contact* c_index = &(cData->contactArray[index]);
-			
-				glm::vec3 linearChange[2], angularChange[2];
-				c_index->applyPositionChange(linearChange,
-											 angularChange,
-											 max);
-			
-				//update contacts due to the resolving interpenetration
-				for (int k = 0; k < cData->contactCount; k++)
-				{
-					Contact* c_k = &(cData->contactArray[k]);
-
-					// Check each body in the contact
-					for (unsigned b = 0; b < 2; b++) if (c_k->body[b])
-					{
-						// Check for a match with each body in the newly
-						// resolved contact
-						for (unsigned d = 0; d < 2; d++)
-						{
-							if (c_k->body[b] == c_index->body[d])
-							{
-								glm::vec3 deltaPosition = linearChange[d] +
-									glm::cross(angularChange[d], c_k->relContactPoint[b]);
-
-								// The sign of the change is positive if we're
-								// dealing with the second body in a contact
-								// and negative otherwise (because we're
-								// subtracting the resolution)..
-								c_k->contactDepth +=
-									glm::dot(deltaPosition, c_k->contactNormal)
-									* (b ? 1 : -1);
-							}
-						}
-					}
-
-					//Contact* cc = &(cData->contactArray[k]);
-					//if (cc->body[0] == b)
-					//{
-						//collisionDetection(cc->body[0], vertex[cc->localContactId], cc);
-					//}
-				}
-			}
-			
-			for (int velIteration = 0; velIteration < 5; ++velIteration)
-			{
-				float max = 0.0f;
-				int index = -1;
-
-				for (int j = 0; j < cData->contactCount; j++)
-				{
-					Contact* c = &(cData->contactArray[j]);
-
-					if (c->body[0] == b)
-					{
-						glm::vec3& N = c->contactNormal;
-						float desiredVel = -(1 + c->restitution) * glm::dot(desiredVelocity(c), N);
-						if (desiredVel > max)
-						{
-							max = desiredVel;
-							index = j;
-						}
-					}
-				}
-
-				if (index != -1)
-				{
-					Contact* c = &(cData->contactArray[index]);
-					resolveFrictionContact(c);
-				}
-			}
+			if (typeOfC == 1)
+				restingContacts.push_back(c);
+			else
+				resolveFrictionContact(c);
 		}
+				
+		//restingContactsHandled(restingContacts);
 	}
 }
