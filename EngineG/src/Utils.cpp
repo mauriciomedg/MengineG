0c12b8add309802d4a81535671a1d4b59403dd14
Mauricio Medina <minimiGamesAM@gmail.com>
2024-02-04 00:57:08 -0500

    Enable mipmapping, to leave openGL generate that automatically.

--- EngineG/src/Utils.cpp
+++ EngineG/src/Utils.cpp
@@ -11,6 +11,17 @@ GLuint Utils::loadTexture(const char* texImagePath)
 	textureID = SOIL_load_OGL_texture(texImagePath,
 		SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
 	if (textureID == 0) cout << "could not find texture file" << texImagePath << endl;
+	
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	//if (glewIsSupported("GL_EXT_texture_filter_anisotropic")) {
+	//	GLfloat anisoSetting = 0.0f;
+	//	glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &anisoSetting);
+	//	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, anisoSetting);
+	//}
+
 	return textureID;
 }
 
