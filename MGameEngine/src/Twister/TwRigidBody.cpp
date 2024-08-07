f887b2577eb0dc5a53ce7b1f01f83c08d26275aa
Mauricio M <mauriciomedg@gmail.com>
2024-08-06 23:13:05 -0400

    better use namespace MG for cpp.

--- MGameEngine/src/Twister/TwRigidBody.cpp
+++ MGameEngine/src/Twister/TwRigidBody.cpp
@@ -1,40 +1,40 @@
 #include "TwRigidBody.h"
 #include <math.h>
 
-namespace MG
+using namespace MG;
+
+TwDynamicBox* TwDynamicBox::create(const TwVec3& halfSize, float mass)
 {
-	TwDynamicBox* TwDynamicBox::create(const TwVec3& halfSize, float mass)
-	{
-		return new TwDynamicBox(halfSize, mass);
-	}
-
-	TwDynamicBox::TwDynamicBox(const TwVec3& halfSize, float mass)
-	{
-		mMass = mass;
-
-		mMassInv = 1 / mass;
-
-		float x0 = 2.0f * halfSize.x;
-		float y0 = 2.0f * halfSize.y;
-		float z0 = 2.0f * halfSize.z;
-
-		mIbody.setDiagonal(std::pow(y0, 2) + std::pow(z0, 2),
-			std::pow(x0, 2) + std::pow(z0, 2),
-			std::pow(x0, 2) + std::pow(y0, 2));
-
-		mIbody *= mMass / 12;
-
-		mIbodyInv = mIbody.inverse();
-	}
-
-	TwRigidBody::TwRigidBody(TwDynamicShape* shape, const TwMat4& transform, bool isStatic)
-		: mShape(shape)
-	{
-	}
-
-	TwRigidBody* TwRigidBody::create(TwDynamicShape* shape, const TwMat4& transform, bool isStatic)
-	{
-		return new TwRigidBody(shape, transform, isStatic);
-	}
+	return new TwDynamicBox(halfSize, mass);
 }
+
+TwDynamicBox::TwDynamicBox(const TwVec3& halfSize, float mass)
+{
+	mMass = mass;
+
+	mMassInv = 1 / mass;
+
+	float x0 = 2.0f * halfSize.x;
+	float y0 = 2.0f * halfSize.y;
+	float z0 = 2.0f * halfSize.z;
+
+	mIbody.setDiagonal(std::pow(y0, 2) + std::pow(z0, 2),
+		std::pow(x0, 2) + std::pow(z0, 2),
+		std::pow(x0, 2) + std::pow(y0, 2));
+
+	mIbody *= mMass / 12;
+
+	mIbodyInv = mIbody.inverse();
+}
+
+TwRigidBody::TwRigidBody(TwDynamicShape* shape, const TwMat4& transform, bool isStatic)
+	: mShape(shape)
+{
+}
+
+TwRigidBody* TwRigidBody::create(TwDynamicShape* shape, const TwMat4& transform, bool isStatic)
+{
+	return new TwRigidBody(shape, transform, isStatic);
+}
+
 
